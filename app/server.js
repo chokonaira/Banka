8f0f3b4c1431fa49face069c729b2008444dec08
chuka okonkwo <okonkwo.henry2012@gmail.com>
2019-04-02 14:27:52 +0100

    ch 164791116 set up dev environment

---
+++ app/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).send(
+    'Banka is a light-weight core banking application that powers banking operations like account creation, customer deposit and withdrawals. This app is meant to support a single bank, where users can signup and create bank accounts online, but must visit the branch to withdraw or deposit money..'
+  );
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app; 
