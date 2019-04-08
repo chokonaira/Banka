82952d82d0c8714c6025bba76e903edd98056580
chuka okonkwo <okonkwo.henry2012@gmail.com>
2019-04-08 15:06:02 +0100

    ft #164791106 user sign-up route

---
+++ app/db/user.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    id: 1,
+    firstname: 'john',
+    lastname: 'doe',
+    email: 'johndoe@gmail.com',
+    password: 'johndoe',
+    type: 'staff',
+    isAdmin: 'true',
+  },
+];
