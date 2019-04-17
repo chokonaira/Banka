3a949f48f34875b29693c3f2603c2d404777f1c7
chuka okonkwo <okonkwo.henry2012@gmail.com>
2019-04-17 14:12:38 +0100

    #165353236 Modified UI pages

--- UI/assets/js/login.js
+++ UI/assets/js/login.js
@@ -4,6 +4,3 @@ signIn.addEventListener('click', (e) => {
 	e.preventDefault();
 	window.location = 'user-dashboard.html';
 });
-
-const mainNav = document.getElementById('js-nav');
-const navBarToggle = document.getElementById('toggler');
