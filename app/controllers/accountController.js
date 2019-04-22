23e1ba96341b803f981489d94dc112d2d21b0f54
chuka okonkwo <okonkwo.henry2012@gmail.com>
2019-04-22 16:16:58 +0100

    feat(Banka): Set up create bank account route set up user create Acount controller module to insert into elephantSQL database [Delivers #165492762]

--- app/controllers/accountController.js
+++ app/controllers/accountController.js
@@ -1,61 +1,46 @@
 /* eslint-disable require-jsdoc */
-import shortid from 'shortid';
-import accountDb from '../db/account';
+import pool from '../db';
+import auth from '../middleware/auth';
 
-export default class Account {
-  static createAccount(req, res) {
-    const {
-      firstname, email, lastname, id,
-    } = req.decoded;
+class Account {
+  static async createAccount(req, res) {
+    const user = auth.tokenBearer(req);
 
-    const ownerId = id;
-    const accountNumber = Math.floor(Math.random() * 1000000000);
-    const date = new Date();
-    const {
-      type, status, openingBalance,
-    } = req.body;
+    // check the status of user ADMIN or USER
+    if (!user.isAdmin && user.type.toLowerCase() === 'user') {
+      const owner = user.userId;
+      console.log(user)
+      const accountNo = Math.floor(Math.random() * 1000000000);
+      const createdOn = new Date();
 
-    if (!type || !status || !openingBalance) {
-      return res.status(400).send({
-        success: 'false',
-        message: 'field required',
-      });
+      const { type, status, openingBalance } = req.body;
+      const createQuery = `INSERT INTO accounts(accountNo, createdOn, owner, type, status, openingBalance)
+                            VALUES ($1, $2, $3, $4, $5, $6) 
+                            RETURNING accountNo, createdOn, owner, type, openingBalance, status`;
+      const values = [accountNo, createdOn, owner, type.trim(), status.trim(), openingBalance];
+
+
+      try {
+        const { rows } = await pool.query(createQuery, values);
+        return res.status(201).send({
+          status: 201,
+          message: 'Account created successfully',
+          data: rows[0],
+        });
+      } catch (error) {
+        return res.status(500).send({
+          status: 500,
+          error: 'Unable to Create Account!! Server Error, Try Again',
+        });
+      }
     }
-
-    const acctExist = accountDb.filter(
-      acct => acct.email === email.toLowerCase(),
-    );
-
-    if (!acctExist.length < 1) {
-      return res.status(409).json({
-        message: 'account already exist',
-      });
-    }
-    const data = {
-      id: shortid.generate(),
-      ownerId,
-      firstname,
-      accountNumber,
-      lastname,
-      email,
-      date,
-      type,
-      status,
-      openingBalance,
-    };
-    accountDb.push(data);
-    return res.status(201).json({
-      status: 201,
-      message: 'Account created successfully',
-      data: {
-        date,
-        accountNumber,
-        firstname,
-        lastname,
-        email,
-        type,
-        openingBalance,
-      },
+    return res.status(401).json({
+      status: 401,
+      error: 'you must be a user to perform this task',
     });
   }
+
+  
 }
+
+export default Account;
